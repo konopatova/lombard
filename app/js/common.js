629081a11df390b57994e79cb2d578d4a543bac3
Anastasia Konopatova <32372877+konopatova@users.noreply.github.com>
2019-09-30 10:31:55 +0300

    section buyout

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
  
+	$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+		nav:true,
+		rewind:true,
+		center:false,
+		mouseDrag: true,
+		navText: ["<img src='./img/arrow-left.png'>","<img src='./img/arrow.png'>"],
+    responsive:{
+        0:{
+						items:1,
+						stagePadding: 90,
+						margin:30,
+						center:true
+        },
+        600:{
+            items:2
+        },
+        800:{
+            items:2
+        },
+        1000:{
+            items:2
+        },
+        1140:{
+						items:4,
+        }
+    }
+})
+
+
 });
 
--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/slick-carousel/slick/slick.min.js',
+		'app/libs/owl/owl.carousel.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
